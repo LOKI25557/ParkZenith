90e284290ebc9321b13e5f9d343f6de497bfd46a
LOKI25557 <lokeshgowdads88@gmail.com>
2026-08-31 20:38:51 +0530

    Feature(auth): add admin authorization for parking operations

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from .auth.router import router as auth_router
 from .users import router as users_router
-from .parking.router import router as parking_router
+from .facilities.router import router as facilities_router
+from .zones.router import router as zones_router
+from .slots.router import router as slots_router
 from .reservation.router import router as reservation_router
 from .analytics.router import router as analytics_router
 from .prediction.router import router as prediction_router
@@ -12,7 +14,9 @@ api_router = APIRouter()
 
 api_router.include_router(auth_router)
 api_router.include_router(users_router)
-api_router.include_router(parking_router)
+api_router.include_router(facilities_router)
+api_router.include_router(zones_router)
+api_router.include_router(slots_router)
 api_router.include_router(reservation_router)
 api_router.include_router(analytics_router)
 api_router.include_router(prediction_router)
